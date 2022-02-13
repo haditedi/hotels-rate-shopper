a517392bf188bd27ce834fb3506e2b547d4461d0
Hadi Tedi <haditedi@gmail.com>
2022-02-13 12:56:57 +0000

    added logo

--- gui.py
+++ gui.py
@@ -11,20 +11,6 @@ from charting import plot_chart
 
 import concurrent.futures
 
-root = tk.Tk()
-
-root.title("HOTEL RATE SHOPPER")
-root.geometry('600x300+50+50')
-
-s= ttk.Style()
-s.theme_use('clam')
-
-root.columnconfigure(0,weight=1)
-root.columnconfigure(1,weight=1)
-root.columnconfigure(2,weight=1)
-
-start_date = tk.StringVar()
-end_date = tk.StringVar()
 
 def calender_func(arg):
     print(arg)
@@ -76,9 +62,26 @@ def handleSubmit():
     plot_chart(h_x_axis, h_y_axis, m_y_axis, s_y_axis)
 
 
+#SETUP AND LAYOUT
+root = tk.Tk()
 
-title = ttk.Label(root, text="HOTEL RATE SHOPPER")
-title.grid(column=1, row=0, columnspan=1,padx=5, pady=50)
+root.title("HOTEL RATE SHOPPER")
+root.geometry('600x450+50+50')
+root.config(bg='#4a7a8c')
+
+s= ttk.Style()
+s.theme_use('clam')
+
+root.columnconfigure(0,weight=1)
+root.columnconfigure(1,weight=1)
+root.columnconfigure(2,weight=1)
+
+start_date = tk.StringVar()
+end_date = tk.StringVar()
+
+img = tk.PhotoImage(file='logo.png')
+logo = ttk.Label(root,image=img,background="grey")
+logo.grid(column=1, row=0, columnspan=1,padx=5, pady=50)
 
 start_button = ttk.Button(root, text='Start Date', command=lambda: calender_func("start"))
 start_button.grid(column=0, row=1, sticky="E", padx=5, pady=5)
@@ -88,5 +91,6 @@ end_button.grid(column=2, row=1, sticky="W", padx=5, pady=5)
 
 submit_button = ttk.Button(root, text='Submit', command=handleSubmit)
 submit_button.grid(column=1, row=3, padx=5, pady=5)
+
 
 root.mainloop()
